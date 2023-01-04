aa63f753689e6371a44ccd40027a388677192844
Satysatyam <121289720+Satysatyam@users.noreply.github.com>
2023-01-04 22:21:32 +0530

    Update loops.java

--- src/loops.java
+++ src/loops.java
@@ -2,5 +2,7 @@ public class loops {
     public static void main(String[] args) {
         System.out.println("satyam");
         System.out.println("after Github");
+        //editing in github
+         System.out.println("Updating in GitHub");
     }
 }
