6baba791abadb4627398a1b797831f42fbbcf87e
Satysatyam <satyamborn2001@gmail.com>
2023-01-06 20:03:14 +0530

    some new updates

--- src/loops.java
+++ src/loops.java
@@ -1,8 +1,24 @@
 public class loops {
     public static void main(String[] args) {
-        System.out.println("satyam");
-        System.out.println("after Github");
-        //editing in github
-         System.out.println("Updating in GitHub");
+//        String a ="0";
+//        String b="a" ;
+//        System.out.println(a.compareTo(b));
+//        / String str1="Coding";
+//        System.out.println(str1.contains("ing"));
+//       StringBuffer str1=new StringBuffer("");
+//for(int i=0;i<5;i++)
+//{
+//    str1.append((char)('a'+i));
+//}
+//System.out.println(str1);
+//}
+        StringBuffer str1=new StringBuffer("");
+        for(int i=0;i<5;i++)
+        {
+            str1.append((char)('a'+i));
+        }
+        System.out.println(str1);
+
+
     }
 }
---
+++ src/practice2dArray/practice2dArray.java
@@ -0,0 +1,9 @@
+package practice2dArray;
+
+public class practice2dArray {
+    public static void main(String[] args) {
+        int[][] arr2d=new int[3][4];
+        arr2d[1][1]=20;
+        System.out.println(arr2d[1][1]);
+    }
+}
