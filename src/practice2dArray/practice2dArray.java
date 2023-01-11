60d6cd2ebf33b0f2332e2450ddbb316bb4522d18
Satysatyam <satyamborn2001@gmail.com>
2023-01-11 19:46:36 +0530

    some new updates

--- src/practice2dArray/practice2dArray.java
+++ src/practice2dArray/practice2dArray.java
@@ -1,9 +1,78 @@
 package practice2dArray;
 
 public class practice2dArray {
+
+//    public static int fun(int[][] arr2d)
+//    {
+//        int sum=0;
+//        for(int i=0;i<arr2d.length;i++)
+//        {
+//            for(int j=0;j<arr2d[i].length;j++)
+//            {
+//                sum+=arr2d[i][j];
+//            }
+//        }
+//        return sum;
+//    }
+
+
+//    public static void fun(int[][] arr2d) {
+//
+//        for (int i = 0; i < arr2d.length; i++) {
+//            for (int j = 0; j < arr2d[i].length; j++) {
+//                arr2d[i][j] = 2 * i + j;
+//            }
+//        }
+//    }
+
     public static void main(String[] args) {
-        int[][] arr2d=new int[3][4];
-        arr2d[1][1]=20;
-        System.out.println(arr2d[1][1]);
+//        int[][] arr2d=new int[3][4];
+//        arr2d[1][1]=20;
+//        System.out.println(arr2d[1][1]);
+
+//.................................................................................................
+
+//        int arr[][]=new int[4][5];
+//        for(int i=0;i<5;i++)
+//        {
+//            arr[i][0]=2;
+//        }
+//        System.out.print(arr[3][0]);
+
+
+//.................................................................................................
+//        int[][] arr = new int[2][2];
+//        for (int i = 0; i < 2; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//        }
+
+//.................................................................................................
+
+
+//        int[][] arr = { {0,1,2,4,5}, {3,4,5,6,7},{5,6,7,8,9} };
+//        System.out.println(fun(arr));
+
+//.................................................................................................
+
+
+//        int[][] arr = new int[2][2];
+//        fun(arr);
+//        for (int i = 0; i < 2; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//
+//
+//        }
+//        int arr[][]={ {1,2,3,4} , {2,4} , {3,5,7,8,9,11} };
+//        System.out.print(arr.length+arr[0].length+
+//                arr[1].length+arr[2].length);
+        int arr[][]=new int [4][];
+        System.out.print(arr[2].length);
+
     }
 }
+
+
